f05ff9cc099a0a929482ec98e4e633e8d688b0ee
Miguel Pardo <migsub77@gmail.com>
2016-12-03 21:21:10 -0800

    fixed links in overlay

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
   let apps = {
     pokedexGo: {
       image: "assets/pokedexLogo.png",
+      github: "https://github.com/MigsAdventure/PokedexGO",
+      preview: "webApps/PokedexGO/pokedexGo.html",
       tech: "jQuery and BootStrap",
       description: "Pokedex Go is a pokedex specifically made for Pokemon Go. A CP calculator is included to see how strong a pokemon will be after evolving. Pokemons also make sounds.",
       reason: "Pokemon Go had just been released. After playing the game for several days, I found it hard to find a CP calculator online. I was determined to build a CP calculator, but somehow ended up making a pokedex with a CP calculator as a feature."
@@ -10,6 +12,8 @@ $(document).ready(function () {
 
     gokuHangman: {
       image: "assets/gokuLogo.png",
+      github: "https://github.com/MigsAdventure/GokuHangman",
+      preview: "webApps/GokuHangMan/gokuHangman.html",
       tech: "jQuery and BootStrap",
       description: "A goku hangman app. There is one letter that is given as a hint. The app will let one know when a letter has been pressed more than once.",
       reason: "I grew up watching dragonball, so I chose the main character as the hangman character."
@@ -25,14 +29,17 @@ $(document).ready(function () {
         $('#appTech').text(apps.pokedexGo.tech);
         $('#appDesc').text(apps.pokedexGo.description);
         $('#appWhy').text(apps.pokedexGo.reason);
+        $('#github').attr('href',apps.pokedexGo.github);
+        $('#preview').attr('href',apps.pokedexGo.preview);
         break;
       case 'Goku_Hangman':
         $('#projectImage').attr('src', apps.gokuHangman.image);
         $('#appTech').text(apps.gokuHangman.tech);
         $('#appDesc').text(apps.gokuHangman.description);
         $('#appWhy').text(apps.gokuHangman.reason);
+        $('#github').attr('href',apps.gokuHangman.github);
+        $('#preview').attr('href',apps.gokuHangman.preview);
         break;
-
       default: "sorry there was a problem"
     }
 
