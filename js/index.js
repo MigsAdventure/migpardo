1460fa30a67605cc238d29d46ff2db496f989e49
Miguel Pardo <migsub77@gmail.com>
2016-12-03 23:43:58 -0800

    added polaroid to portfolio

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
 
   let apps = {
+    polaroidImage: {
+      image: "assets/polaroidImage.png",
+      github: "https://github.com/MigsAdventure/polaroid-image",
+      preview: "https://www.npmjs.com/package/polaroid-image",
+      tech: "React and Babel",
+      description: "Turn any image into a polaroid! This is an npm package to be used with react projects. The user can easily add an image to the component to create a polaroid. There's props that make it easy to rotate the image and change how much it zooms on hover. There's also props to change the frame color and box shadows. ",
+      reason: "Since the day I found out I could upload my own npm package, I knew this would be my first package. I tend to use polaroids throughout my projects. I wanted to create something useful for myself that would also benefit others. Learning how to create and upload it was also a fun experience. "
+    },
     pokedexGo: {
       image: "assets/pokedexLogo.png",
       github: "https://github.com/MigsAdventure/PokedexGO",
@@ -24,6 +32,14 @@ $(document).ready(function () {
     let currApp =  $(this).attr('alt');
 
     switch(currApp) {
+      case 'Polaroid_Image':
+        $('#projectImage').attr('src', apps.polaroidImage.image);
+        $('#appTech').text(apps.polaroidImage.tech);
+        $('#appDesc').text(apps.polaroidImage.description);
+        $('#appWhy').text(apps.polaroidImage.reason);
+        $('#github').attr('href',apps.polaroidImage.github);
+        $('#preview').attr('href',apps.polaroidImage.preview);
+        break;
       case 'Pokedex_Go':
         $('#projectImage').attr('src', apps.pokedexGo.image);
         $('#appTech').text(apps.pokedexGo.tech);
