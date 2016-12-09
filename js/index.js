bb6ce46ab0140c7df2ad3a3f7439ddb8669403d4
Miguel Pardo <migsub77@gmail.com>
2016-12-08 16:45:59 -0800

    added new project

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
 
   let apps = {
+    memoriesAlbum: {
+      image: "assets/memoriesAlbum.png",
+      github: "https://github.com/johnsalay/memories",
+      preview: "http://memoriesalbum.herokuapp.com/",
+      tech: "React, Flux, Bootstrap, Flexbox, MySQL and Heroku.",
+      api: "Microsoft Cognitive Services Computer Vision API, Emotion API, and Bing Image Search API.",
+      description: "Search for images and add them to an album. User can also add images to album using an image url. There is also a view that shows current trending images. The album uses a MySQL database. Currently, there are no user accounts, therefore all images added to album will be shared between all users. This app is still a work in progress.",
+      reason: "A friend of mine and I found out about Microsoft Cognitive Services. We wanted to practice using their API. I built a basic app using polaroids with jQuery in the past and he wanted to create a photo album. Thus, we developed this app."
+    },
     polaroidImage: {
       image: "assets/polaroidImage.png",
       github: "https://github.com/MigsAdventure/polaroid-image",
       preview: "https://www.npmjs.com/package/polaroid-image",
       tech: "React and Babel",
+      api: '',
       description: "Turn any image into a polaroid! This is an npm package to be used with react projects. The user can easily add an image to the component to create a polaroid. There's props that make it easy to rotate the image and change how much it zooms on hover. There's also props to change the frame color and box shadows. ",
       reason: "Since the day I found out I could upload my own npm package, I knew this would be my first package. I tend to use polaroids throughout my projects. I wanted to create something useful for myself that would also benefit others. Learning how to create and upload it was also a fun experience. "
     },
@@ -14,6 +24,7 @@ $(document).ready(function () {
       github: "https://github.com/MigsAdventure/PokedexGO",
       preview: "webApps/PokedexGO/pokedexGo.html",
       tech: "jQuery and BootStrap",
+      api: '',
       description: "Pokedex Go is a pokedex specifically made for Pokemon Go. A CP calculator is included to see how strong a pokemon will be after evolving. Pokemons also make sounds.",
       reason: "Pokemon Go had just been released. After playing the game for several days, I found it hard to find a CP calculator online. I was determined to build a CP calculator, but somehow ended up making a pokedex with a CP calculator as a feature."
     },
@@ -22,6 +33,7 @@ $(document).ready(function () {
       github: "https://github.com/MigsAdventure/GokuHangman",
       preview: "webApps/GokuHangMan/gokuHangman.html",
       tech: "jQuery and BootStrap",
+      api: '',
       description: "A goku hangman app. There is one letter that is given as a hint. The app will let one know when a letter has been pressed more than once.",
       reason: "I grew up watching dragonball, so I chose the main character as the hangman character."
     }
@@ -30,19 +42,20 @@ $(document).ready(function () {
   $('.infoBtn').on('click', function() {
     let currApp =  $(this).attr('alt');
 
-      $('#projectImage').attr('src', apps[currApp].image);
-      $('#appTech').text(apps[currApp].tech);
-      $('#appDesc').text(apps[currApp].description);
-      $('#appWhy').text(apps[currApp].reason);
-      $('#github').attr('href',apps[currApp].github);
-      $('#preview').attr('href',apps[currApp].preview);
+    $('#projectImage').attr('src', apps[currApp].image);
+    $('#appTech').text(apps[currApp].tech);
+    $('#appApi').text(apps[currApp].api);
+    $('#appDesc').text(apps[currApp].description);
+    $('#appWhy').text(apps[currApp].reason);
+    $('#github').attr('href',apps[currApp].github);
+    $('#preview').attr('href',apps[currApp].preview);
 
     $('.hideInfo').addClass('detailContainer');
 
   });
 
   $('.closeBtn').on('click',function() {
-      $('.hideInfo').removeClass('detailContainer');
+    $('.hideInfo').removeClass('detailContainer');
   });
 
 
